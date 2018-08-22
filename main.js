63275b5af2934cce1550b1961b24afcd0600bb3e
Dustin Dubia <ddubia@gmail.com>
2018-08-22 10:28:46 -0700

    populateGameBoardAdded

---
+++ main.js
@@ -0,0 +1,15 @@
+var gameBoardArray = [];
+
+function populateGameBoardArray(boardSize) {
+    gameBoardArray = [];
+    var counter = 0;
+    for (var i = 0; i <= boardSize-1; i++) {
+        gameBoardArray.push([counter]);
+        counter++;
+        for (var x = 0; x <= boardSize-2; x++) {
+            gameBoardArray[i].push(counter);
+            counter++;
+        }
+    }
+}
+
